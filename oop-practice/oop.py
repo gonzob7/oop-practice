fa59556d19feedffe91fc53e63d2388b2606a473
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-18 17:16:32 -0700

    added age property

--- oop-practice/oop.py
+++ oop-practice/oop.py
@@ -1,12 +1,14 @@
 class Person(object):
-    def __init__(self, person_name, hair_color):
+    def __init__(self, person_name, hair_color, age):
         self.name = person_name
         self.hairColor = hair_color
+        self.age = age
     def say_hello(self):
         print("Hi everyone! My name is {}".format(self.name))
         print("My hair color is {}".format(self.hairColor))
+        print("My age is {}".format(self.age))
 
-John = Person("Gonzo","Black")
+John = Person("Gonzo","Black","18")
 
 
 John.say_hello()
