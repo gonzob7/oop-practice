2d7522488634fe8c3cdda4570876f7c324b6b9bb
Gonzalo Birrueta <silentcr7@gmail.com>
2019-09-23 17:12:45 -0700

    added student subclass of person with add course and get course methods

--- oop-practice/oop.py
+++ oop-practice/oop.py
@@ -1,14 +1,32 @@
 class Person(object):
-    def __init__(self, person_name, hair_color, age):
+    def __init__(self, person_name):
         self.name = person_name
-        self.hairColor = hair_color
-        self.age = age
+
     def say_hello(self):
         print("Hi everyone! My name is {}".format(self.name))
-        print("My hair color is {}".format(self.hairColor))
-        print("My age is {}".format(self.age))
 
-John = Person("Gonzo","Black","18")
+class Student(Person):
+    def __init__(self, name):
+        super().__init__(name)
+        self.courses = []
 
+    def add_course(self, course):
+        self.courses.append(course)
 
-John.say_hello()
+    def get_courses(self):
+        count = len(self.courses)
+        print(f"I'm taking {count} courses")
+        for course in self.courses:
+            print(f"   -{course}")
+
+class Teacher(Person):
+    pass
+
+Jane = Student("Jane")
+Jane.say_hello()
+Jane.get_courses()
+
+Jane.add_course("CS1.1")
+Jane.add_course("BEW1.1")
+Jane.add_course("SPD1.1")
+Jane.get_courses()
